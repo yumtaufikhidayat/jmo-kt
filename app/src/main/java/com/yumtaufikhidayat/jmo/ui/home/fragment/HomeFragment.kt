3257c64dc5bebc1f780a923eeee299e86be329a9
yumtaufikhidayat <yumtaufik1997@gmail.com>
2023-04-25 09:13:17 +0700

    Handle back pressed to exit.

--- app/src/main/java/com/yumtaufikhidayat/jmo/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/jmo/ui/home/fragment/HomeFragment.kt
@@ -1,9 +1,13 @@
 package com.yumtaufikhidayat.jmo.ui.home.fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -22,8 +26,27 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<HomeViewModel>()
+    private val delayTime = 2000L
+    private var doubleBackToExitPressedOnce = false
     private val serviceProgramAdapter by lazy { ServiceProgramAdapter() }
     private val otherServiceAdapter by lazy { OtherServiceAdapter() }
+
+    private val backPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (doubleBackToExitPressedOnce) {
+                requireActivity().finish()
+                return
+            }
+
+            doubleBackToExitPressedOnce = true
+            Toast.makeText(requireContext(), "Ketuk sekali lagi untuk keluar ", Toast.LENGTH_SHORT).show()
+
+            Handler(Looper.getMainLooper())
+                .postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, delayTime)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +59,11 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            backPressedCallback
+        )
+
         initServiceProgramAdapter()
         initOtherProgramAdapter()
         setServiceProgramData()
